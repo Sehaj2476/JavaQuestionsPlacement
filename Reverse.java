6ff9f6c0ee4dee5530792fe38076ab41cdd3d757
Sehaj2476 <2002iamsehaj@gmail.com>
2026-09-15 15:28:11 +0530

    added reverse of an array

---
+++ Reverse.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Reverse{
+    public static void reverseArray(int arr[], int n){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        } 
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        reverseArray(arr,n);
+        for(int i=0;i<n ; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
